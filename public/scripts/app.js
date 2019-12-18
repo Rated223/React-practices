c9a24afaef18bd824a6661fb2b335e912b8a244b
Daniel Carrizales <rated223@outlook.com>
2019-12-17 23:56:55 -0600

    Adding functionality to the indecision app Implementing rendering of an array
    Signed-off-by: Daniel Carrizales <rated223@outlook.com>

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,37 +4,51 @@ console.log('App.js is running.');
 var appInfo = {
   title: 'Indecision App',
   subtitle: 'This is some info',
-  options: ['one', 'two']
-};
-var template = React.createElement("div", null, React.createElement("h1", null, appInfo.title), appInfo.subtitle && React.createElement("p", null, appInfo.subtitle), React.createElement("p", null, appInfo.options && appInfo.options.length > 0 ? 'Here are your options' : 'No options'), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
-var count = 0;
-
-var addOne = function addOne() {
-  count++;
-  renderCounterApp();
+  options: []
 };
 
-var minusOne = function minusOne() {
-  count--;
-  renderCounterApp();
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var optionInput = e.target.elements.option.value;
+
+  if (optionInput) {
+    appInfo.options.push(optionInput);
+    e.target.elements.option.value = '';
+    render();
+  }
 };
 
-var reset = function reset() {
-  count = 0;
-  renderCounterApp();
+var removeAll = function removeAll(e) {
+  e.preventDefault();
+  appInfo.options = [];
+  render();
+};
+
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * appInfo.options.length);
+  var option = appInfo.options[randomNum];
+  console.log(option);
 };
 
 var appRoot = document.querySelector('#app');
 
-var renderCounterApp = function renderCounterApp() {
-  var templateTwo = React.createElement("div", null, React.createElement("h1", null, "Count: ", count), React.createElement("button", {
-    onClick: addOne
-  }, "+1"), React.createElement("button", {
-    onClick: minusOne
-  }, "-1"), React.createElement("button", {
-    onClick: reset
-  }, "reset"));
-  ReactDOM.render(templateTwo, appRoot);
+var render = function render() {
+  var template = React.createElement("div", null, React.createElement("h1", null, appInfo.title), appInfo.subtitle && React.createElement("p", null, appInfo.subtitle), React.createElement("button", {
+    onClick: onMakeDecision,
+    disabled: appInfo.options.length === 0
+  }, "what should I do?"), React.createElement("p", null, appInfo.options && appInfo.options.length > 0 ? 'Here are your options' : 'No options'), React.createElement("ol", null, appInfo.options.map(function (option, index) {
+    return React.createElement("li", {
+      key: index
+    }, option);
+  })), React.createElement("form", {
+    onSubmit: onFormSubmit
+  }, React.createElement("input", {
+    type: "text",
+    name: "option"
+  }), React.createElement("button", null, "Add"), React.createElement("button", {
+    onClick: removeAll
+  }, "Remove all")));
+  ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
--- src/app.js
+++ src/app.js
@@ -3,51 +3,56 @@ console.log('App.js is running.');
 const appInfo = {
   title: 'Indecision App',
   subtitle: 'This is some info',
-  options: [
-    'one',
-    'two'
-  ]
+  options: []
 }
 
-const template = (
-  <div>
-    <h1>{ appInfo.title }</h1>
-    { appInfo.subtitle && <p>{ appInfo.subtitle }</p> }
-    <p>{ ( appInfo.options && appInfo.options.length > 0 ) ? 'Here are your options' : 'No options' }</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-};
+  const optionInput = e.target.elements.option.value;
+  if (optionInput) {
+    appInfo.options.push(optionInput);
+    e.target.elements.option.value = '';
+    render();
+  }
+}
+
+const removeAll = (e) => {
+  e.preventDefault();
+  appInfo.options = [];
+  render();
+}
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * appInfo.options.length);
+  const option = appInfo.options[randomNum];
+  console.log(option);
+
+}
 
 const appRoot = document.querySelector('#app');
 
-const renderCounterApp = () => {
-  const templateTwo = (
+const render = () => {
+  const template = (
     <div>
-      <h1>Count: { count }</h1>
-      <button onClick={ addOne }>+1</button>
-      <button onClick={ minusOne }>-1</button>
-      <button onClick={ reset }>reset</button>
+      <h1>{ appInfo.title }</h1>
+      { appInfo.subtitle && <p>{ appInfo.subtitle }</p> }
+      <button onClick={ onMakeDecision } disabled={ appInfo.options.length === 0 }>what should I do?</button>
+      <p>{ ( appInfo.options && appInfo.options.length > 0 ) ? 'Here are your options' : 'No options' }</p>
+      <ol>
+        { 
+          appInfo.options.map((option, index) => <li key={index}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={ onFormSubmit }>
+        <input type="text" name="option"/>
+        <button>Add</button>
+        <button onClick={ removeAll }>Remove all</button>
+      </form>
     </div>
   );
-  
-  ReactDOM.render(templateTwo, appRoot);
-};
+    
+  ReactDOM.render(template, appRoot);
+}
 
-renderCounterApp();
+render();
