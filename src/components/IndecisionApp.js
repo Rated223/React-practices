86a1fb3434761570b43d40b2039b2c7e6041750e
Daniel Carrizales <rated223@outlook.com>
2020-01-15 00:30:36 -0600

    Implementing webpack in the project Installing the react libraries with yarn modulating the app by components
    Signed-off-by: Daniel Carrizales <rated223@outlook.com>

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  AddOption,
+  Header,
+  Action,
+  Options
+} from '.';
+
+export default class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    
+    this.state = {
+      options: []
+    };
+  }
+
+  componentDidMount() {
+    try {  
+      const json = localStorage.getItem('options');
+  
+      if (json) {
+        const options = JSON.parse(json);
+        this.setState(() => ({ options }))
+      }
+    } catch(e) {
+      
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.options.length !== prevState.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => ({ options: [] }));
+  }
+
+  handleDeleteOption(optionSelected) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => option !== optionSelected)
+    }))
+  }
+
+  handlePick() {
+    const choise = Math.floor(Math.random() * this.state.options.length);
+    alert(this.state.options[choise]);
+  }
+
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter a valid value to add item.';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists.';
+    }
+
+    this.setState((prevState) => ({ options: prevState.options.concat([option]) }));
+  }
+
+  render() {
+    const subtitle = 'Put your life in the hands of a computer';
+
+    return (
+      <div>
+        <Header subtitle={ subtitle }/>
+        <Action 
+          hasOptions = { this.state.options.length > 0 } 
+          handlePick = {this.handlePick}
+        />
+        <Options 
+          options = { this.state.options } 
+          handleDeleteOptions = {this.handleDeleteOptions}
+          handleDeleteOption = {this.handleDeleteOption}
+        />
+        <AddOption 
+          handleAddOption = {this.handleAddOption}
+        />
+      </div>
+    );
+  }
+}
+
+IndecisionApp.defaultProps = {
+  options: []
+}
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import IndecisionApp from './IndecisionApp';
+import AddOption from './AddOption';
+import Option from './Option';
+import Header from './Header';
+import Action from './Action';
+import Options from './Options';
+
+export {
+  IndecisionApp,
+  AddOption,
+  Option,
+  Header,
+  Action,
+  Options
+}
