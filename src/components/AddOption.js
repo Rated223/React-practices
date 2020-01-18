c0dacbd9272662d1c802481672f2936739d3d705
Daniel Carrizales <rated223@outlook.com>
2020-01-18 02:10:49 -0600

    Finishing the styles
    Signed-off-by: Daniel Carrizales <rated223@outlook.com>

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -20,9 +20,13 @@ export default class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={ this.handleAddOption } action="">
-          <input type="text" name="option" id="option"/>
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+        <form
+           className="add-option"
+          onSubmit={ this.handleAddOption } 
+          action=""
+        >
+          <input className="add-option__input" type="text" name="option" id="option"/>
           <button className="button">Add Option</button>
         </form>
       </div>
